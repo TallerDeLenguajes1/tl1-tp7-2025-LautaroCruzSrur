b961bab0f585eff6b77e287676d4fd6385f08976
Lautaro G Cruz Srur <105937766+LautaroCruzSrur@users.noreply.github.com>
2025-05-31 21:42:33 -0300

    corrigiendo errores

--- SisPersonal/Empleado.cs
+++ SisPersonal/Empleado.cs
@@ -5,25 +5,72 @@ public class Empleado
     private string Nombre;
     private string Apellido;
 
-    private datetime FechaNacimiento;
+    private DateTime FechaNacimiento;
 
     private char EstadoCivil;
-    private dateTime FechaDeIngresoEnLaEmpresa;
+    private DateTime FechaDeIngresoEnLaEmpresa;
     private double SueldoBasico;
 
-    private enum Cargos
+    public enum Cargos
     {
-        Administrativo = 1;
-        Ingeniero = 2;
-        Especialista = 3;
-        Investigador = 4;
-
+        Administrativo = 1,
+        Ingeniero = 2,
+        Especialista = 3,
+        Investigador = 4,
     }
 
-    public datetime CalcularAntiguedad() {
-        
+    public int CalcularAntiguedad(DateTime FechaDeIngresoEnLaEmpresa)
+    {
+        DateTime hoy = DateTime.Today;
+        int Antiguedad = hoy.Year - FechaDeIngresoEnLaEmpresa.Year;
+
+        if (hoy.Month < FechaDeIngresoEnLaEmpresa.Month || (hoy.Month == FechaDeIngresoEnLaEmpresa.Month && hoy.Day < FechaDeIngresoEnLaEmpresa.Day))
+        {
+            Antiguedad--;
     }
-    public datetime CalcularEdad() {
-        
+
+        return Antiguedad;
+    }
+    public int CalcularEdad(DateTime FechaNacimiento)
+    {
+        DateTime hoy = DateTime.Today;
+        int edad = hoy.Year - FechaNacimiento.Year;
+
+        if (hoy.Month < FechaNacimiento.Month || (hoy.Month == FechaNacimiento.Month && hoy.Day < FechaNacimiento.Day))
+        {
+            edad--;
+    }
+
+        return edad;
+
+    }
+    public int Jubilacion(int edad)
+    {
+        int JubilacionEdad = edad - 65;
+
+        return JubilacionEdad;
+    }
+    public double Salario(double SueldoBasico, int Antiguedad, Cargos Cargo)
+    {
+        double Adicional = 0;
+        if (Antiguedad > 20)
+        {
+            Adicional += (SueldoBasico * 0.25);
+        }
+        else
+        {
+            Adicional += ((SueldoBasico * 0.01) * Antiguedad);
+        }
+        if (Cargo == Cargos.Ingeniero || Cargo == Cargos.Especialista)
+        {
+            Adicional += (Adicional * 0.50);
+        }
+        if (EstadoCivil == 'C')
+        {
+            Adicional += 150000;
+        }
+        double salario = SueldoBasico + Adicional;
+
+        return salario;
     }
 }
